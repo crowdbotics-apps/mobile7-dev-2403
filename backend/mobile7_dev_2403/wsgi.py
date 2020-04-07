8604d826f8d5cb9f2fce188bea96f1f2dd65c1ad
Crowdbotics <team@crowdbotics.com>
2020-04-07 08:23:10 +0000

    Initial commit.

---
+++ backend/mobile7_dev_2403/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for mobile7_dev_2403 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mobile7_dev_2403.settings')
+
+application = get_wsgi_application()
